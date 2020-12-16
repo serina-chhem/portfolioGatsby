e494aab51f80dec8eb51b22a988cee39d82af6ae
Serina <serina.chhem@gmail.com>
2020-12-16 16:25:16 +0100

    Added image to Hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,9 +1,9 @@
 import React, {useState} from "react"
-// import styles from "../styles/styles.scss"
-// import Image from "../components/image"
-import styled, {ThemeProvider, createGlobalStyle} from 'styled-components'
+import styled from 'styled-components'
+
 import Typical from 'react-typical'
-import Toggle from "./toggleDarkLight";
+import { useStaticQuery, graphql } from 'gatsby';
+import BackgroundImage from "gatsby-background-image";
 
 
 
@@ -11,13 +11,24 @@ const StyledHero = styled.section`
     flex-direction: column;
     align-items: flex-start;
     min-height: auto;
+    .inner {
+    padding-top:40px;
+
+    display: grid;
+    grid-template-columns: 3fr 2fr;
+    grid-gap: 50px;
+    @media (max-width: 768px) {
+      display: block;
+    }
+    
+  }
 
     h1 {
         margin: 0 0 30px 4px;
         margin-top: 20px;
         color: var(--bleu-vert);
         line-height: 1.5;
-        font-size: 3rem;
+        font-size: 2.7rem;
         font-weight: 600;
         letter-spacing:5px;
         @media (max-width: 480px) {
@@ -40,19 +51,68 @@ h3{
 
 
 
-const Hero = () => {
+const StyledPic = styled.div`
+  position: relative;
+  max-width: auto;
+  @media (max-width: 768px) {
+    margin: 50px auto 0;
+    width: 70%;
+  }
 
+  .img {
+        filter: none;
+      position: relative;
+      margin:auto auto;
+      mix-blend-mode: normal;
+      width:27rem;
+      height:27rem;
+      
+    }
+
+  .wrapper {
+    display: block;
+    position: relative;
+    width: 100%;
+   
+  }
+
+`
+
+
+const Hero = () => {
+    const data = useStaticQuery(graphql`
+    {
+        file(relativePath: {eq: "christmasHero.svg"}) {
+            extension
+            id
+            publicURL
+            base
+          }
+    }
+  `)
 
     return(
 
 
 
-        <StyledHero>
+        <StyledHero >
+            
             <h3 className={"medium-heading"}>Bienvenue ! Je suis Sérina,</h3>  
-
-        <Typical steps={['Une étudiante déterminée,', 200, 'Une étudiante déterminée, adaptable', 200, 'Une étudiante déterminée, adaptable et prête à créer des expériences utilisateurs uniques.', 200]} loop="4" wrapper="h1"/> 
+           
+           <div className="inner"> 
+           
+            {/* <Typical steps={['Une étudiante déterminée,', 200, 'Une étudiante déterminée, adaptable', 200, 'Une étudiante déterminée, adaptable et prête à créer des expériences utilisateurs uniques.', 200]} loop="4" wrapper="h1"/>  */}
+            <h1>
+                Déterminée, adaptable et prête à créer des expériences utilisateurs uniques.
+            </h1>
+            
+            <StyledPic>
+                <img className = "img" src={data.file.publicURL} alt={data.base}/>
+            </StyledPic>
+           
+            </div>
             <h2 className="medium-heading">Si vous recherchez une stagiaire en fin d’études à Paris, en tant que UX/UI Designer, ou Product Owner, je serais ravie de vous aider à maximiser la valeur de votre produit !</h2>
-        
+            
         </StyledHero>
 
        
